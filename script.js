839504f9afe6802fdfa36fba4d7dc37ebe74ae11
Japji Multani <15006996+jay23bueller@users.noreply.github.com>
2024-03-18 14:12:49 -0700

    Add comment to createGrid function

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ resizeButtonElement.addEventListener("click", () => {
     createGrid(gridSize);
 });
 
-
+//Take into consderation the additional space that is taken
+// by the border, gap, and padding
 function createGrid(gridSize = 16) {
     for(let i = 0; i < gridSize; i++) {
         for(let j = 0; j < gridSize; j++) {
